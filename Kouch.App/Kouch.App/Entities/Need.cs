ef188f305c090d8dfc34e2ffa7598fc97c6c219e
Mityaechka <50111604+Mityaechka@users.noreply.github.com>
2021-05-03 22:06:07 +0600

    0305

---
+++ Kouch.App/Kouch.App/Entities/Need.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kouch.App.Entities
+{
+    public class Need
+    {
+        public int Id { get; set; }
+        public string    Title { get; set; }  
+
+    }
+}
